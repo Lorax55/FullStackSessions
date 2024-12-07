fa4159090cadda5523f9d2b7294ac6a8b0cf175e
Lorax55 <vladyslav.usa@gmail.com>
2024-12-06 19:01:57 -0800

    first update

---
+++ .idea/src/Session2/Practice.java
@@ -0,0 +1,14 @@
+package Session2;
+
+public class Practice {
+    public static void main(String[] args) {
+        int  age = 25;
+        double height = 5.11;
+        String name = "Vlad";
+        System.out.println(name);
+
+        System.out.println("My name is " + name + ", I am " + age + " years old, and I am " + height + " foot tall.");
+    }
+
+
+}
---
+++ .idea/src/Session2/StudentDetails.java
@@ -0,0 +1,34 @@
+package Session2;
+
+import java.util.Scanner;
+
+public class StudentDetails {
+    public static void main(String[] args) {
+        // Student identifier
+        // Student first name
+        // Student last name
+        // Student age
+
+        // Student identifier
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter student identifier: ");
+        int studentId = scanner.nextInt();
+        System.out.println("Student ID: " + studentId);
+        String firstName = scanner.nextLine();
+        System.out.println("Enter student first name: ");
+        String lastName = scanner.next();
+        System.out.println("Enter student last name: ");
+        int age = scanner.nextInt();
+
+        System.out.println("\n ===== Student details =====");
+        System.out.println("Student ID: " + studentId);
+        System.out.println("First name: " + firstName);
+        System.out.println("Last name: " + lastName);
+        System.out.println("Age: " + age);
+        System.out.println("====== Student Detilas ======");
+        scanner.close();
+
+
+    }
+}
